a00fad16091bd84f70296a84bd0832b1d6306b44
rpoteau <romuald.poteau@univ-tlse3.fr>
2026-04-01 13:23:14 +0200

    Bump version: 0.9.0 → 0.9.1

--- pyphyschemtools/__init__.py
+++ pyphyschemtools/__init__.py
@@ -3,7 +3,7 @@
 The pyphyschemtools library provides a comprehensive suite of utilities for Physical Chemistry, ranging from spectroscopic unit management to kinetic modeling and cheminformatics.
 """
 
-__version__ = "0.9.0"
+__version__ = "0.9.1"
 __last_update__ = "2026-04-01"
 
 import importlib
